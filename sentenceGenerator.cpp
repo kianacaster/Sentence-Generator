9b7c6b7da4c4cebf8f1b2b9a0e69d45a404f65be
Kian Acaster <acasterkian@gmail.com>
2017-07-21 00:13:48 +0100

    Update sentenceGenerator.cpp

--- sentenceGenerator.cpp
+++ sentenceGenerator.cpp
@@ -9,7 +9,7 @@ string generateSentence()
     const char* nouns[10] = {"cat", "dog", "table", "car", "gun", "person", "man", "woman", "TV", "meth lab"}; 
     const char* verbs[10] = {"laughed", "ran", "ate", "cooked", "shot", "fought", "walked", "burned", "entered", "left"}; 
     const char* adjectives[10] = {"big","stupid","hard","dangerous","nice","threatening","ignorant","short","loud","boring"};
-    const char* adverbs[10] = {"slowly","strangely","randomly","unexpectedly","happily","seductively","amazingly","quickly","arrogently","violently"};
+    const char* adverbs[10] = {"slowly","strangely","randomly","unexpectedly","happily","seductively","amazingly","quickly","arrogantly","violently"};
     
     return std::string("The ") + adjectives[rand() % sizeof(adjectives-1)] + std::string(" ") + nouns[rand() % sizeof(nouns-1)] + std::string(" ") + verbs[rand() % sizeof(verbs-1)] + std::string(" ") + adverbs[rand() % sizeof(adverbs-1)] + std::string(".");  
 }
